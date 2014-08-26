5f64c115744a12baa7e575964ad346b16000a60c
Hardy Lou <hlou65@gmail.com>
2014-08-25 23:28:17 -0700

    Refactor strings
    Moved hardcoded strings in XML files into strings.xml

--- gen/com/sb/studybuddies/R.java
+++ gen/com/sb/studybuddies/R.java
@@ -1254,9 +1254,18 @@ containing a value of this type.
         /**  Description of a share target (both in the list of such or the default share button) in a ShareActionProvider (share UI). [CHAR LIMIT=NONE] 
          */
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
-        public static final int action_settings=0x7f0a000f;
-        public static final int app_name=0x7f0a000d;
-        public static final int hello_world=0x7f0a000e;
+        public static final int action_settings=0x7f0a0010;
+        public static final int app_name=0x7f0a000e;
+        public static final int email=0x7f0a0013;
+        public static final int first_name=0x7f0a0011;
+        public static final int hello_world=0x7f0a000d;
+        public static final int last_name=0x7f0a0012;
+        public static final int login=0x7f0a0015;
+        public static final int password=0x7f0a0014;
+        public static final int register=0x7f0a0016;
+        public static final int splash_text=0x7f0a000f;
+        public static final int to_login=0x7f0a0018;
+        public static final int to_register=0x7f0a0017;
     }
     public static final class style {
         /** 
